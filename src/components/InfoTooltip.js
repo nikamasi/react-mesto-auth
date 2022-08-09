4dfa429233dcccc7aabf0f6384b06c4a2f10f61d
Nika Masimova <masimostudy#gmail.com>
2022-08-09 17:40:49 +0300

    fix: infotooltip success logo

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -13,8 +13,8 @@ function InfoTooltip(props) {
         <div className="pop-up__form">
           <img
             className="pop-up__logo"
-            src={props.success ? success : error}
-            alt={props.success ? "символ ок" : "символ ошибки"}
+            src={props.message.success ? success : error}
+            alt={props.message.success ? "символ ок" : "символ ошибки"}
           ></img>
           <h3 className="pop-up__title pop-up__title_auth">
             {props.message.text}
